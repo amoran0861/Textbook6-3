ccc50fa860ba6a41fd4f6e0ea46c948dca18f5c4
amoran0861 <59611622+amoran0861@users.noreply.github.com>
2020-10-01 17:34:29 -0500

    Add files via upload

---
+++ Fraction.java
@@ -0,0 +1,23 @@
+
+public class Fraction
+{
+    private int num;
+    private int dem;
+
+    public Fraction(int num, int dem)
+    {
+        int numerator = num;
+        int denominator = dem;
+    }
+    
+    public int getNum()
+    {
+        return num;
+    }
+    
+    public int getDem()
+    {
+       return dem;
+    }
+    
+}
