da4a9999b72d99c46f7e81f75d8774474a5266a8
amoran0861 <59611622+amoran0861@users.noreply.github.com>
2020-10-04 16:30:06 -0500

    Finished

--- Fraction.java
+++ Fraction.java
@@ -3,6 +3,9 @@ public class Fraction
 {
     private int numerator;
     private int denominator;
+    private int mathBottom;
+    private int mathTop;
+
 
     public Fraction(int num, int den)
     {
@@ -20,4 +23,32 @@ public class Fraction
        return denominator;
     }
     
+    public void addFrac(Fraction tempFrac)
+    {
+       mathBottom = denominator * tempFrac.getDem();
+       mathTop = numerator * tempFrac.getDem() + tempFrac.getNum() * denominator;
+       System.out.println(mathTop + "/" + mathBottom);
+    }
+    
+    public void subFrac(Fraction tempFrac)
+    {
+       mathBottom = denominator * tempFrac.getDem();
+       mathTop = numerator * tempFrac.getDem() - tempFrac.getNum() * denominator;
+       System.out.println(mathTop + "/" + mathBottom);
+    }
+    
+    public void multFrac(Fraction tempFrac)
+    {
+        mathBottom = denominator * tempFrac.getDem();
+        mathTop = numerator * tempFrac.getNum();
+        System.out.println(mathTop + "/" + mathBottom);
+    }
+    
+    
+    public void divFrac(Fraction tempFrac)
+    {
+        mathTop = numerator * tempFrac.getDem();
+        mathBottom = denominator * tempFrac.getNum();
+        System.out.println(mathTop + "/" + mathBottom);
+    }
 }
--- main.java
+++ main.java
@@ -3,5 +3,19 @@ public class main
 {
 
     public static void main (String[] args){
+        //FRACTION MAKER
+        Fraction oneHalf = new Fraction(1,2);
+        Fraction oneThird = new Fraction(1,3);
+        Fraction oneFourth = new Fraction(1,4);
+        
+        //FRACTION TESTER
+        System.out.println("Adding 1/2 and 1/3");
+        oneHalf.addFrac(oneThird);
+        System.out.println("Subtracting 1/4 from 1/2");
+        oneHalf.subFrac(oneFourth);
+        System.out.println("Multiplying 1/2 by itself");
+        oneHalf.multFrac(oneHalf);
+        System.out.println("Dividing 1/3 by 1/4");
+        oneThird.divFrac(oneFourth);
     }
 }
